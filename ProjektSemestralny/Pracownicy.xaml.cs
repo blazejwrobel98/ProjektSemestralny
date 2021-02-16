e02f13f04dbafa06691046011015bdfb1b8bd436
Błażej Wróbel <blazej.wrobel@microsoft.wsei.edu.pl>
2021-02-16 21:58:56 +0100

    ADD PracownicyClass as DB operator

--- ProjektSemestralny/Pracownicy.xaml.cs
+++ ProjektSemestralny/Pracownicy.xaml.cs
@@ -19,9 +19,20 @@ namespace ProjektSemestralny
     /// </summary>
     public partial class Pracownicy : Window
     {
+        PracownicyClass Pclass = new PracownicyClass();
         public Pracownicy()
         {
             InitializeComponent();
+            this.Pracownicy_table.ItemsSource = Pclass.CreateTable();
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Pracownicy_table.Columns[0].Visibility = Visibility.Collapsed;
+            this.Pracownicy_table.Columns[1].Header = "Imię";
+            this.Pracownicy_table.Columns[2].Header = "Nazwisko";
+            var counter = this.Pracownicy_table.Columns.Count();
+            this.Pracownicy_table.Columns[counter-1].Visibility = Visibility.Collapsed;
+            this.Pracownicy_table.Columns[counter-2].Visibility = Visibility.Collapsed;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
---
+++ ProjektSemestralny/PracownicyClass.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjektSemestralny
+{
+    class PracownicyClass
+    {
+        Database1Entities db = new Database1Entities();
+        /// <summary>
+        /// Pobieranie danych z tabeli Pracownik
+        /// </summary>
+        /// <returns>Pobieranie danych z tabeli Pracownik</returns>
+        public List<Pracownik> CreateTable() => db.Pracownik.ToList();
+    }
+}
